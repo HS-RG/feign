3504879c64a05ddd7ac97c0844e3d809e958db84
wxl <1715142548@qq.com>
2023-06-07 17:26:59 +0800

    file的feign增加参数

--- src/main/java/com/hsrg/clients/FileClient.java
+++ src/main/java/com/hsrg/clients/FileClient.java
@@ -2,11 +2,12 @@ package com.hsrg.clients;
 
 import com.hsrg.pojo.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient("file")
 public interface FileClient {
-    @PostMapping("/file/uploadImage")
+    @PostMapping(value = "/file/uploadImage",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     Result uploadImage(MultipartFile image);
 }
