4eb3811a26702e612df22dcbb6efa20384d56bda
Cbgogogog <1293543528@qq.com>
2023-06-13 00:30:28 +0800

    修改收藏接口

--- src/main/java/com/hsrg/clients/FileClient.java
+++ src/main/java/com/hsrg/clients/FileClient.java
@@ -1,13 +1,18 @@
 package com.hsrg.clients;
 
+import com.hsrg.pojo.File;
 import com.hsrg.pojo.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient("file")
 public interface FileClient {
-    @PostMapping(value = "/file/uploadImage",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/file/uploadImage", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     Result uploadImage(MultipartFile image);
+
+    @PostMapping("/file/QueryFileByFileId")
+    Result QueryFileByFileId(@RequestBody File file);
 }
